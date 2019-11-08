09e0bbf0aa2067adc0ca35b93e215dfbfde16ddb
Yamil Emmanuel Fajouri <fajouri.yamil@gmail.com>
2019-11-08 16:22:45 +0100

    Decorator
    Decorator

---
+++ Decorator/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Decorator
+{
+    class DecoratorPattern
+    {
+
+        interface IComponent
+        {
+            string Operation();
+        }
+
+        class Component : IComponent
+        {
+            public string Operation()
+            {
+                return "I am walking";
+            }
+        }
+
+        class DecoratorA : IComponent
+        {
+            private IComponent component;
+
+            public DecoratorA(IComponent _component)
+            {
+                component = _component;
+            }
+
+            public string Operation()
+            {
+                var componentOperation = component.Operation();
+                componentOperation += " and listening to Classic FM";
+                return componentOperation;
+            }
+        }
+
+        class DecoratorB: IComponent
+        {
+            private IComponent component;
+            public string addedState = " past the Coffe Shop";
+
+            public DecoratorB(IComponent _component)
+            {
+                component = _component;
+            }
+
+            public string Operation()
+            {
+                return component.Operation() + " to school";
+            }
+
+            public string AddedBehavior()
+            {
+                return " and I bought a cappuccino ";
+            }
+        }
+        static void Display(string s, IComponent c)
+        {
+            Console.WriteLine(s +  c.Operation());
+        }
+        
+        static void Main(string[] args)
+        {
+            Console.Write("Decorator Pattern\n");
+            IComponent component = new Component();
+            Display("1. Basic Component: ", component);
+            Display("2. A-decorated: ", new DecoratorA(component));
+            Display("3. B-decorated: ", new DecoratorB(component));
+            Display("4. B-A-decorated: ", new DecoratorB(new DecoratorA(component)));
+            var b = new DecoratorB(new Component());
+            Display("5. A-B-decorated: ", new DecoratorA(b));
+            Console.WriteLine(("\t\t\t"+b.addedState + b.AddedBehavior()));
+            Console.ReadLine();
+
+        }
+    }
+
+}
